7f51a39abfa4eb1284e35e2dfc02710b8d8d5436
Lubo387 <lubomiks387@gmail.com>
2026-09-25 21:39:44 +0300

    8

---
+++ GrowablePatientManager.cs
@@ -0,0 +1,93 @@
+﻿namespace ClinicApp;
+
+public class GrowablePatientManager
+{
+    private Patient[] _patients = new Patient[4];
+    private int _count = 0;
+
+    public int Count => _count;
+    public int Capacity => _patients.Length;
+
+    private void Grow()
+    {
+        int oldCapacity = _patients.Length;
+        int newCapacity = oldCapacity * 2;
+        Patient[] newArray = new Patient[newCapacity];
+
+        for (int i = 0; i < _count; i++)
+        {
+            newArray[i] = _patients[i];
+        }
+
+        _patients = newArray;
+        Console.WriteLine($"  Масив заповнений! Розширення: {oldCapacity} → {newCapacity}");
+    }
+
+    public void Add(Patient patient)
+    {
+        if (_count == _patients.Length)
+        {
+            Grow();
+        }
+
+        _patients[_count] = patient;
+        _count++;
+    }
+
+    public Patient? FindById(int id)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            if (_patients[i].Id == id)
+            {
+                return _patients[i];
+            }
+        }
+
+        return null;
+    }
+
+    public bool Remove(int id)
+    {
+        int index = -1;
+
+        for (int i = 0; i < _count; i++)
+        {
+            if (_patients[i].Id == id)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            return false;
+        }
+
+        for (int i = index; i < _count - 1; i++)
+        {
+            _patients[i] = _patients[i + 1];
+        }
+
+        _count--;
+        _patients[_count] = null!;
+        return true;
+    }
+
+    public void DisplayAll()
+    {
+        Console.WriteLine($"=== Пацієнти ({_count} / {Capacity}) ===");
+
+        if (_count == 0)
+        {
+            Console.WriteLine("Список порожній.");
+            return;
+        }
+
+        for (int i = 0; i < _count; i++)
+        {
+            Console.WriteLine(_patients[i]);
+        }
+    }
+}
